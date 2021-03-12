c1989a94574547631ba0ea55124c470383a2ebc6
meghna.mehta <meghna.mehta@thegatewaycorp.co.in>
2021-03-12 10:41:20 +0530

    assignment

---
+++ ClassLibrary1/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringOperations
+{
+    public static class Operations
+    {
+        public static string ConvertUpperLower(string x) {
+            string y = "";
+            for(int i = 0; i < x.Length; i++)
+            {
+                char ch = x[i];
+                if (ch >= 'A' && ch <= 'Z') {
+                    int p = (int)ch;
+                    p = p + 32;
+                    y += (char)p;
+                }
+                else if(ch>='a' && ch<='z')
+                {
+                    int q=(int)ch;
+                    q = q - 32;
+                    y += (char)q;
+                }
+                else
+                {
+                    y += ch;
+                }
+            }
+            return y;
+        }
+    }
+}
---
+++ StringTesting/TestOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StringOperations;
+using Xunit;
+
+namespace StringTesting
+{
+    public class TestOperations
+    {
+        [Fact]
+        public void Test_ConvertUpperLower()
+        {
+            // Arrange
+            var x = "MeGhNa";
+            var Expect = "mEgHnA";
+
+            // Act
+            var result = Operations.ConvertUpperLower(x);
+            // Assert
+            Assert.Equal(Expect, result);
+        }
+    }
+}
